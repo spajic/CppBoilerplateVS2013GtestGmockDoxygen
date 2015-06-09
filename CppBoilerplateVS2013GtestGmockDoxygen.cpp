127bcf613ed30912eab21e27c6e5ba95c426d4ea
spajic <bestspajic@gmail.com>
2015-06-09 21:25:33 +0300

    create first working console application

---
+++ CppBoilerplateVS2013GtestGmockDoxygen.cpp
@@ -0,0 +1,16 @@
+// CppBoilerplateVS2013GtestGmockDoxygen.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "Hello, boilerplate!\n";
+	std::cout << "Press enter to exit.";
+	std::cin.get();
+	return 0;
+}
+
