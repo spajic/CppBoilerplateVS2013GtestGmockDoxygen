c15aa50cecad9151db5fad3356fa757cbd0350c5
spajic <bestspajic@gmail.com>
2015-06-10 15:28:42 +0300

    add powershell script to perform renames from BoilerplateName to ProjectName

---
+++ RenameBoilerplate.ps1
@@ -0,0 +1,20 @@
+#Перед запуском этого скрипта каталог с проектом нужно переименовать с BoilerplateName на новое название ProjectName.
+#Скрипт осуществляет замену BoilerplateName на ProjectName в названиях и в тексте всех файлов (помимо этого скрипта).
+#После работы скрипта (но не ранее!) Soulution ProjectName.sln можно открывать в VS2013 и собирать.
+#Названия всех проектов и связанных сущностей в VS будут соответствовать ProjectName.
+
+$BoilerplateName = 'CppBoilerplateVS2013GtestGmockDoxygen'
+$ThisScriptName = 'RenameBoilerplate.ps1'
+$ProjectName = (Get-ItemProperty $ThisScriptName).Directory.Name
+
+#Переименовываем файлы
+Dir -Filter "$($BoilerPlateName)*" | Rename-Item  -NewName { $_.name -replace $BoilerPlateName, $ProjectName }
+
+#Осуществляем замену в текстах файлов
+$files=Dir -Exclude $ThisScriptName -Recurse
+foreach ($file in $files) 
+{
+    (Get-Content $file.PSPath) | 
+    Foreach-Object {$_ -replace $BoilerPlateName, $ProjectName} |
+    Set-Content $file.PSPath
+}
